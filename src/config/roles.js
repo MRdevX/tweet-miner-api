c26a10c6aba540117a385a58bad8c13aeecf16cf
Mahdi Rashidi <m8rashidi@gmail.com>
2020-10-18 09:06:58 +0330

    update roles

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,8 +1,8 @@
 const roles = ['user', 'admin']
 
 const roleRights = new Map()
-roleRights.set(roles[0], ['manageTweets', 'getTweets'])
-roleRights.set(roles[1], ['getUsers', 'manageUsers'])
+roleRights.set(roles[0], ['getUsers', 'manageTweets', 'getTweets'])
+roleRights.set(roles[1], ['getUsers', 'manageUsers', 'manageTweets', 'getTweets'])
 
 module.exports = {
     roles,
